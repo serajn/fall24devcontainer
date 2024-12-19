ae4ac4fdb72120093db09899274d84b28928a722
serajn <snoureddine01@outlook.com>
2024-12-19 02:27:31 +0000

    add date

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-           Seraj get started by editing{" "}
+           Seraj get started by editing on 12/18/2024{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               app/page.tsx
             </code>
